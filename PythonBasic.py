3bd056d47424607be5b0286808f2082f9c0ae10b
Students <philipphilipov28@itpg-varna.bg>
2025-09-17 14:51:43 +0300

    PythonBasic1

---
+++ PythonBasic.py
@@ -0,0 +1,13 @@
+a=5
+b=10
+print(a+b)
+ime= input("kak se kazvash:")
+print("Zdravej",ime)
+
+print("kak se kazvash:")
+ime2= input()
+print("Zdravej ", ime2)
+
+spisak=[1,2,3,4,5]
+print(spisak)
+print([x**2 for x in spisak])
